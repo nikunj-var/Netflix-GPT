e85464e87e4fefc83cd42b2ccfdfd313365dfe84
Nikunj <nikunjvar2002@gmail.com>
2024-01-14 19:01:30 +0530

    sign in and sign up form done

--- netflix-gpt/README.md
+++ netflix-gpt/README.md
@@ -4,6 +4,8 @@
 
 - Create React app.
 - Configured Tailwind CSS
+- Header
+- Login Form
 
 ...
 
--- netflix-gpt/src/components/Login.js
+++ netflix-gpt/src/components/Login.js
@@ -1,10 +1,47 @@
+import { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignIn, setIsSignIn] = useState(true);
+  const handleToggleSignIn = () => {
+    setIsSignIn(!isSignIn);
+  };
   return (
     <div>
       <Header />
-      <img src="https://assets.nflxext.com/ffe/siteui/vlv3/594f8025-139a-4a35-b58d-4ecf8fdc507c/d3c4e455-f0bf-4003-b7cd-511dda6da82a/IN-en-20240108-popsignuptwoweeks-perspective_alpha_website_large.jpg" />
+      <div className="absolute">
+        {" "}
+        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/594f8025-139a-4a35-b58d-4ecf8fdc507c/d3c4e455-f0bf-4003-b7cd-511dda6da82a/IN-en-20240108-popsignuptwoweeks-perspective_alpha_website_large.jpg" />
+      </div>
+      <div>
+        {" "}
+        <form className="text-white absolute w-4/12 p-12  bg-black bg-opacity-75 mx-auto right-0 left-0  my-20 ">
+          <h1 className="font-bold text-3xl py-2 m-2">
+            {isSignIn ? "Sign in" : "Sign Up"}
+          </h1>
+         {!isSignIn && <input
+            type="text"
+            placeholder="Full Name"
+            className="p-3 m-2 bg-gray-700 w-full rounded"
+          />}
+          <input
+            type="text"
+            placeholder="Email Address"
+            className="p-3 m-2 bg-gray-700 w-full rounded"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="p-3 m-2 w-full bg-gray-700 rounded"
+          />
+          <button className="p-3 m-2 mt-5 mb-32 bg-red-500 w-full rounded">
+            {isSignIn ? "Sign in" : "Sign Up"}
+          </button>
+          <p className="p-3 m-2 cursor-pointer" onClick={handleToggleSignIn}>
+            {isSignIn ? "New to Netflix? Sign Up Now" : "Already registered? Sign In Now" }
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
